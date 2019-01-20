bbdd88f05fa1938b5116b6f2dfffb76c1937f1a0
Lucas Stoller <l.s.stoller@gmail.com>
2019-01-20 16:42:53 -0200

    Bugfix: category validation dont work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,6 @@ class PostsController < ApplicationController
 
     private
         def permitted_params
-            params.require(:post).permit(:title, :body, :thumbnail, :category)
+            params.require(:post).permit(:title, :body, :thumbnail, :category_id)
         end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,14 @@
-class Post < ActiveRecord::Base    
+class Post < ActiveRecord::Base
+    belongs_to :category
+
     validates :title, presence: { in: true, message: "O post deve ter um título, mané!" }
     validates :body, presence: { in: true, message: "Como vai postar algo sem conteúdo, vacilão?" }
     validates :thumbnail, presence: { in: true, message: "Uma imagem vale mais que mil palavras." }
-    validates :category, presence: { in: true, message: "Custa nada escolher uma categoria pro teu post, mano..." }
-
-    belongs_to :category
-
+    validates :category_id, presence: { in: true, message: "Custa nada escolher uma categoria pro teu post, mano..." }
+    
     mount_uploader :thumbnail, ThumbnailUploader
+
+    def to_s
+        "#{self.title}\n#{self.body}\n#{self.thumbnail}\n#{self.category_id}"
+    end
 end
